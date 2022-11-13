57e29fa5d0ca5227cb7e77256b3f9cccc52d23ef
R <crvl102@outlook.com>
2022-11-13 08:49:10 +0000

    j

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const HomePage = () => {
-  return <div className="text-3xl text-green-600 p-2">Hello Geeks!</div>;
+  return (
+    <div className="text-3xl font-AzoBlack text-green-600 p-2">
+      Hello Geeks!
+    </div>
+  );
 };
 
 export default HomePage;
